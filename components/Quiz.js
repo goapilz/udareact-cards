cbb42e26fc8ec3a1ccc12b34c488ac59ca0f58e3
goapilz <markus_29@gmx.de>
2017-12-10 22:16:33 +0100

    quiz requirements

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import {View, Text, TouchableOpacity, TextInput, StyleSheet} from 'react-native'
-import styled from 'styled-components/native'
+import {View, Text, Alert, TouchableOpacity, TextInput, StyleSheet, Animated} from 'react-native'
 import {connect} from 'react-redux'
-import {addOrUpdateDeck} from '../actions'
-import {gray, white, orange, lightGray} from '../constants/colors'
+import {gray, white, green, red} from '../constants/colors'
 
 const styles = StyleSheet.create({
     view: {
@@ -51,77 +49,90 @@ class Quiz extends React.Component {
     state = {
         questionIndex: 0,
         correctAnswers: 0,
-        showAnswerPressed: false
+        showAnswer: false,
+        bounceValue: new Animated.Value(1),
     }
 
-    showAnswer() {
-        this.setState({showAnswerPressed: true})
+    toggleQuestion() {
+        this.setState({showAnswer: !this.state.showAnswer})
+        this.animate()
+    }
+
+    animate() {
+        const {bounceValue} = this.state
+        Animated.sequence([
+            Animated.timing(bounceValue, {duration: 200, toValue: 1.04}),
+            Animated.spring(bounceValue, {toValue: 1, friction: 4})
+        ]).start()
     }
 
     correct() {
         let {questionIndex, correctAnswers} = this.state
         questionIndex++
         correctAnswers++
-        this.setState({showAnswerPressed: false, questionIndex, correctAnswers})
+        this.setState({showAnswer: false, questionIndex, correctAnswers})
+        this.animate()
     }
 
     incorrect() {
         let {questionIndex} = this.state
         questionIndex++
-        this.setState({showAnswerPressed: false, questionIndex})
+        this.setState({showAnswer: false, questionIndex})
+        this.animate()
     }
 
     render() {
         const {deck} = this.props
-        const {questionIndex, correctAnswers, showAnswerPressed} = this.state
+        const {questionIndex, correctAnswers, showAnswer, bounceValue} = this.state
         const {goBack} = this.props
 
         // check if quiz should be run
         let terminateQuiz = false
         if (deck.questions.length === 0) {
             terminateQuiz = true
-            alert('no questions')
+            Alert.alert(
+                'No questions', 'Add a question first.',
+                [{text: 'OK', onPress: goBack}],
+                {cancelable: true, onDismiss: goBack}
+            )
         } else if (questionIndex >= deck.questions.length) {
             terminateQuiz = true
-            alert('quiz finished')
+            Alert.alert(
+                `Quiz finished`, `${correctAnswers} correct answers (${Math.round(correctAnswers / deck.questions.length * 100)}%)`,
+                [{text: 'OK', onPress: goBack}],
+                {cancelable: true, onDismiss: goBack}
+            )
         }
 
         if (terminateQuiz) {
-            return (
-                <View style={styles.view}>
-                    <TouchableOpacity
-                        style={styles.btn}
-                        onPress={goBack}><Text style={styles.btnText}>Back</Text></TouchableOpacity>
-                </View>
-            )
+            return (<View style={styles.view}/>)
         } else {
             const question = deck.questions[questionIndex]
             return (
                 <View style={styles.view}>
                     <Text style={styles.textSmall}>Card {questionIndex + 1}/{deck.questions.length}</Text>
-                    <Text style={styles.textSmall}>{Math.round(correctAnswers / deck.questions.length * 100)}% correct
-                        answers</Text>
-                    <Text style={[styles.text, {marginTop: 20}]}>Question:</Text>
-                    <Text style={styles.text}>{question.question}</Text>
-                    {showAnswerPressed ? (<Text style={styles.text}>{question.answer}</Text>) : (<TouchableOpacity
-                        style={styles.btn}
-                        onPress={() => {
-                            this.showAnswer()
-                        }}><Text style={styles.btnText}>Show answer</Text></TouchableOpacity>)}
-                    <Text style={styles.textSmall}>Answer:</Text>
-                    <Text style={styles.textSmall}>{question.answer}</Text>
+                    <Text style={styles.textSmall}>{correctAnswers} correct answers ({Math.round(correctAnswers / deck.questions.length * 100)}%)</Text>
+                    <Animated.Text
+                        style={[styles.text, {marginTop: 20, transform: [{scale: bounceValue}]}]}>
+                        {showAnswer ? question.answer : question.question}
+                    </Animated.Text>
+
+                    <TouchableOpacity style={[styles.btn, {height: 20}]} onPress={() => {
+                        this.toggleQuestion()
+                    }}><Text style={styles.btnText}>Show {showAnswer ? 'question' : 'answer'}</Text></TouchableOpacity>
+
                     <TouchableOpacity
-                        style={styles.btn}
+                        style={[styles.btn, {marginTop: 20, backgroundColor: green}]}
                         onPress={() => {
                             this.correct()
                         }}><Text style={styles.btnText}>Correct</Text></TouchableOpacity>
                     <TouchableOpacity
-                        style={styles.btn}
+                        style={[styles.btn, {marginTop: 20, backgroundColor: red}]}
                         onPress={() => {
                             this.incorrect()
                         }}><Text style={styles.btnText}>Incorrect</Text></TouchableOpacity>
                     <TouchableOpacity
-                        style={styles.btn}
+                        style={[styles.btn, {marginTop: 50}]}
                         onPress={goBack}><Text style={styles.btnText}>Back</Text></TouchableOpacity>
                 </View>
             )
@@ -140,7 +151,6 @@ function mapStateToProps(decks, props) {
 
 function mapDispatchToProps(dispatch, props) {
     return {
-        reloadDecks: () => dispatch(reloadDecks()),
         goBack: () => props.navigation.goBack()
     }
 }
